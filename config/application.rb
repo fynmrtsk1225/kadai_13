69db1c672f56361c75ab93da6a9793f9353c8b65
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-15 03:39:06 +0900

    :wrench: 不要なファイルが生成されないよう記述を追加＆gemを削除

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,23 @@ module Myru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.stylesheets false
+      g.javascripts true
+      g.test_framework false
+    end
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                      model_specs: true,
+                      view_specs: false,
+                      helper_specs: false,
+                      routing_specs: false,
+                      controller_specs: false,
+                      request_specs: false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
