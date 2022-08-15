4de90aa41ac79fa4554f96848041a60921a94c38
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-15 15:15:31 +0900

    :sparkles: 管理者機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root "homes#index"
   devise_for :users, path: 'user'
   resources :homes, only: [:index]
