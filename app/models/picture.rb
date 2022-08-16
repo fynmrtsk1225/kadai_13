5e37f266f261e11c9186fa22d76fcff10234cc15
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-16 14:13:46 +0900

    :wrench: pictureとtagのアソシエーションを修正

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,5 +2,7 @@ class Picture < ApplicationRecord
   belongs_to :user
   has_many :picture_tags, dependent: :destroy
   has_many :attach_tags, through: :picture_tags, source: :tag
+  has_many :tags, through: :picture_tags
+  accepts_nested_attributes_for :picture_tags, allow_destroy: true
   mount_uploader :image, ImageUploader
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
   has_many :picture_tags, dependent: :destroy
   has_many :attach_pictures, through: :picture_tags, source: :picture
+  has_many :pictures, through: :picture_tags
 end
