ddc86f24dd1ae90e0079e496c11dd47859c76973
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-18 07:22:28 +0900

    :wrench: 検索に必要なrootを定義

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   root "homes#index"
   devise_for :users, controllers: { registrations: 'users/registrations' }, path: 'user'
   resources :homes, only: [:index]
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    collection do
+      get 'search'
+    end
+  end
   resources :profiles, only: [:edit, :update]
   resources :pictures
 end
