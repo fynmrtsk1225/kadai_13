1132c46f218a91ae0f56a9e6243be28c70b54e2e
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-17 17:53:39 +0900

    :cake::wrench: productモデル作成＆pictureとアソシエーション

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,5 +5,7 @@ class Picture < ApplicationRecord
   has_many :attached_tags, through: :picture_tags, source: :tag
   accepts_nested_attributes_for :picture_tags, allow_destroy: true
 
+  has_many :products, dependent: :destroy
+
   mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_16_041027) do
+ActiveRecord::Schema.define(version: 2022_08_17_084640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 2022_08_16_041027) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.bigint "picture_id", null: false
+    t.string "name", null: false
+    t.text "product_url", null: false
+    t.text "image_url"
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["picture_id"], name: "index_products_on_picture_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -69,5 +80,6 @@ ActiveRecord::Schema.define(version: 2022_08_16_041027) do
   add_foreign_key "picture_tags", "pictures"
   add_foreign_key "picture_tags", "tags"
   add_foreign_key "pictures", "users"
+  add_foreign_key "products", "pictures"
   add_foreign_key "profiles", "users"
 end
