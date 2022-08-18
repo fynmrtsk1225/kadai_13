345977b1419e4e16b47d49772bd9bf04caa0d480
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-18 11:14:51 +0900

    :wrench: 検索に必要なsearchアクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :sign_in_required, only: [:index, :show]
+  before_action :set_q, only: [:index, :search]
   
   def index
     @users = User.all
@@ -7,5 +8,15 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
+  end
+
+  def search
+    @results = @q.result
+  end
+
+  private
+
+  def set_q
+    @q = User.ransack(params[:q])
   end
 end
