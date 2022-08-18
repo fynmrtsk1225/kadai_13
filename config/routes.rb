7f7f9131a85e58614cc0c4d371380721fa5c8c65
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-18 13:59:20 +0900

    :wrench: productにsearchルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,25 @@
 Rails.application.routes.draw do
-  resources :room_ideas
-  resources :products, only: [:index, :show]
-  resources :tags
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root "homes#index"
-  devise_for :users, controllers: { registrations: 'users/registrations' }, path: 'user'
+
   resources :homes, only: [:index]
+
+  devise_for :users, controllers: { registrations: 'users/registrations' }, path: 'user'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :users, only: [:index, :show] do
     collection do
       get 'search'
     end
   end
+
   resources :profiles, only: [:edit, :update]
+
   resources :pictures
+  resources :tags
+  resources :products, only: [:index, :show] do
+    collection do
+      get 'search'
+    end
+  end
+
+  resources :room_ideas
 end
