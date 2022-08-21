069c5e572b6989351956b32855947315e4e58280
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-22 06:56:32 +0900

    :wrench: ゲストユーザの削除を制御

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :ensure_normal_user, only: %i[update destroy]
 
-  # GET /resource/sign_up
+  def ensure_normal_user
+    if resource.email == 'guest@example.com'||'guest_admin@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーの更新・削除はできません。'
+    end
+  end
+    
   def new
     @user = User.new
     @profile = @user.build_profile
