4b3d1961330e0f2f02ac37764f82b15a162e9a18
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-21 15:40:26 +0900

    :cake: favorite_pictureモデルを作成

---
+++ app/models/favorite_product.rb
@@ -0,0 +1,2 @@
+class FavoriteProduct < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_19_055036) do
+ActiveRecord::Schema.define(version: 2022_08_21_063822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2022_08_19_055036) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["picture_id"], name: "index_favorite_pictures_on_picture_id"
     t.index ["user_id"], name: "index_favorite_pictures_on_user_id"
+  end
+
+  create_table "favorite_products", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "picture_tags", force: :cascade do |t|
