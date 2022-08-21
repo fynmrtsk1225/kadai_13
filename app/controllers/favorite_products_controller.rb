90f7af38f9a6ae4dbc52b552a3c7ef0e62a1f94f
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-21 16:56:17 +0900

    :sparkles: 同期お気に入りの実装

--- app/controllers/favorite_products_controller.rb
+++ app/controllers/favorite_products_controller.rb
@@ -3,8 +3,12 @@ class FavoriteProductsController < ApplicationController
   end
 
   def create
+    FavoriteProduct.create(user_id: current_user.id, product_id: params[:id])
+    redirect_to products_path
   end
 
   def destroy
+    FavoriteProduct.find_by(user_id: current_user.id, product_id: params[:id]).destroy
+    redirect_to products_path
   end
 end
