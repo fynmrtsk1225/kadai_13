20295844e3c9c66b0c8d6308d344772cabf5c4db
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-23 05:25:20 +0900

    :wrench: picture.rb/content-imageにvalidatesを追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,6 @@ class Picture < ApplicationRecord
   belongs_to :user
 
   has_many :picture_tags, dependent: :destroy
-  # has_many :attached_tags, through: :picture_tags, source: :tag
   accepts_nested_attributes_for :picture_tags, allow_destroy: true
 
   has_many :tags, through: :picture_tags
@@ -11,6 +10,9 @@ class Picture < ApplicationRecord
   accepts_nested_attributes_for :products, allow_destroy: true
 
   has_many :favorite_pictures, dependent: :destroy
+
+  validates :image, presence: true
+  validates :content, length: { maximum: 255}
 
   mount_uploader :image, ImageUploader
 end
