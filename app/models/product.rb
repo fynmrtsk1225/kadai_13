c3e1caa3f0d711675f7eac3b6a190ecba4c533e1
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-23 05:31:44 +0900

    :wrench: product.rb/name-product_urlにvalidatesを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,8 @@ class Product < ApplicationRecord
 
   has_many :favorite_products, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 50}
+  validates :product_url, presence: true, format: /\A#{URI::regexp(%w(http https))}\z/
 
   mount_uploader :image, ImageUploader
 end
