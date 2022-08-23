3b1d3b1045beb30837164d5e568245ebf4d42fb3
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-24 04:47:32 +0900

    :wrench: profile編集に制限を追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
   before_action :sign_in_required
   before_action :set_profile
+  before_action :ensure_user
 
   def edit
   end
@@ -16,7 +17,13 @@ class ProfilesController < ApplicationController
   private
 
   def set_profile
-    @profile = current_user.profile
+    @user = User.find(params[:id])
+    @profile = @user.profile
+  end
+
+  def ensure_user
+    @users = current_user
+    redirect_to user_path(@users), notice: "他ユーザのプロフィール更新はできません" if @user != @users
   end
 
   def profile_params
