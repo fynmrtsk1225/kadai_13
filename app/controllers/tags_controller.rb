6b142a7049127c9062525cb0f3fadb1e672a9b5f
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-08-29 15:38:04 +0900

    :lipstick: view-tagsのデザインを最適化

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -26,7 +26,7 @@ class TagsController < ApplicationController
     @tag = Tag.new(tag_params)
 
     if @tag.save
-      redirect_to @tag, notice: 'Tag was successfully created.'
+      redirect_to @tag, notice: 'タグが新規作成されました！'
     else
       render :new
     end
@@ -35,7 +35,7 @@ class TagsController < ApplicationController
   # PATCH/PUT /tags/1
   def update
     if @tag.update(tag_params)
-      redirect_to @tag, notice: 'Tag was successfully updated.'
+      redirect_to @tag, notice: 'タグが更新されました！'
     else
       render :edit
     end
@@ -44,7 +44,7 @@ class TagsController < ApplicationController
   # DELETE /tags/1
   def destroy
     @tag.destroy
-    redirect_to tags_url, notice: 'Tag was successfully destroyed.'
+    redirect_to tags_url, notice: 'タグが削除されました！'
   end
 
   private
